4ba45c4b46bc38f4d7e04a5afbef036921678c2a
monica <monibrave1@gmail.com>
2023-05-09 16:37:19 -0300

    add comment this branch content option api VERSION

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {createApp} = Vue;
-
+//version option API
 const app = createApp({
+    //data() todo lo que contiene data corresponde a un proxy, todo lo que contiene data es observado por vue para reaccionar y ejecutar cuando alguna propiedad cambie
     data(){
         return{
             product: {
