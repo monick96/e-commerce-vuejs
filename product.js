3feff3685bfd8cda829bbaba077e51c6d4e06e09
monica <monibrave1@gmail.com>
2023-05-22 13:31:31 -0300

    Computed properties con Options API

--- product.js
+++ product.js
@@ -35,7 +35,7 @@ app.component("product",{
         return {
             activeImage: 0,
             discountCodes:["commerce50","commerce20"],
-            price_color:"rgb(104, 104, 209)"
+            //price_color:"rgb(104, 104, 209)"
         }
     },
     methods: {
@@ -55,10 +55,18 @@ app.component("product",{
         activeImage(value,oldValue){
             console.log(value,oldValue);
         },
-        "product.stock"(stock){
-            if (stock <= 1){
-                this.price_color= "rgb(188 30 67)";
+        // "product.stock"(stock){
+        //     if (stock <= 1){
+        //         this.price_color= "rgb(188 30 67)";
+        //     }
+        // }
+    },
+    computed: {
+        price_color(){
+            if (this.product.stock<=1){
+                return "rgb(188 30 67)";
             }
+            return "rgb(104, 104, 209)"
         }
     }
 })
